66a9516d1de9cb1e3eba716725901af229fb54f6
liuqing520it <330663384@qq.com>
2017-10-12 22:34:56 +0800

    初始化工程

---
+++ SimpleGames/SimpleGames/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SimpleGames
+//
+//  Created by liuqing on 2017/10/12.
+//  Copyright © 2017年 liuqing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+    }
+}
+
